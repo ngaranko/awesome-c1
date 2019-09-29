5dcf24796dc2e65bc2b6d02a7a742c5930879d91
Nick Garanko <ngaranko@gmail.com>
2019-09-29 22:57:11 +0200

    up

--- rc.lua
+++ rc.lua
@@ -54,7 +54,7 @@ end
 -- beautifuls define colours, icons, font and wallpapers.
 
 -- This is used later as the default terminal and editor to run.
-terminal = "xfce4-terminal"
+terminal = "gnome-terminal"
 editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
 
